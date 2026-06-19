e648900edb883863206b99e54a257f3d95241e9d
Moin Khan <48323683+m01n008@users.noreply.github.com>
2026-06-19 16:14:41 +0500

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.kt
@@ -0,0 +1,27 @@
+class Solution {
+fun groupAnagrams(strs: Array<String>): List<List<String>> {
+    /*Optimized
+    Time Complexity: O(m x n)
+    Space complexity: O(n)
+    */
+    val hashMap = HashMap<String,MutableList<String>>()
+       
+    for (word in strs) {
+       val freq = Array(26){0}  
+       for (ch in word.toCharArray()) {
+           freq[ch - 'a']++
+       }
+        val key = freq.joinToString("#");   
+        if (hashMap[key] == null) { 
+           hashMap[key] = mutableListOf()    
+        }
+        hashMap[key]!!.add(word) 
+    }
+    return hashMap.values.toList()    
+
+}
+
+
+
+
+}
